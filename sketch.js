7d209e702acc5f469d97b98913ee9e80eb473e79
cyberbits99 <79581339+cyberbits99@users.noreply.github.com>
2021-03-23 11:57:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var packageBody,ground;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -56,9 +56,9 @@ function draw() {
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-
-    
+	Matter.Body.setStatic(packageBody, false);
+     }
+ 
   }
 }
 
